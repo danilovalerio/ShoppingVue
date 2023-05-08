c9512b48c60cd6ddca82e177f1518fabf674b772
danilovalerio <danilo_vs@hotmail.com>
2023-05-08 15:45:26 -0300

    criando a view page

---
+++ src/views/Product.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="product">
+
+    Essa página é dos produtos
+    
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    name: 'Product',
+  
+    methods: {
+     
+    },
+   
+  }
+  </script>
+  
+  <style lang="scss">
+
+  </style>
+  
