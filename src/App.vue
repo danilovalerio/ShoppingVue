6793617fd86e7f2736286a893a7faab352c6e98b
danilovalerio <danilo_vs@hotmail.com>
2023-05-08 16:32:50 -0300

    adicionando axios e carregando dados de testes no hook created

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,57 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Início</router-link> - 
-    <router-link to="/basket">Carrinho (0)</router-link> - 
-    <router-link to="/product">Produto</router-link> 
-  </div>
-  <router-view/>
+<div id="nav">
+    <router-link to="/">Início</router-link> -
+    <router-link to="/basket">Carrinho (0)</router-link> -
+    <router-link to="/product">Produto</router-link>
+</div>
+<router-view />
 </template>
 
 <script>
+import axios from 'axios'
 
-  export default {
+export default {
 
-  }
-  
+    created() {
+        axios
+            .get('https://fakestoreapi.com/products')
+            .then(response => {
+                console.log(response.data)
+            })
+    }
+
+}
 </script>
-
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  max-width: 1280px;
-  margin: 80px auto;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    max-width: 1280px;
+    margin: 80px auto;
 }
 
 #nav {
-  padding: 16px 0;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  width: 100%;
-  text-align: center;
-  background-color: rgb(37, 37, 37);
-  color: white;
-
-
-  a {
+    padding: 16px 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    width: 100%;
+    text-align: center;
+    background-color: rgb(37, 37, 37);
     color: white;
-    text-decoration: none;
 
-    &.router-link-exact-active {
-      color: #007bff;
+    a {
+        color: white;
+        text-decoration: none;
+
+        &.router-link-exact-active {
+            color: #007bff;
+        }
     }
-  }
 }
 </style>
