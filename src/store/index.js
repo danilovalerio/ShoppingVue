50a1465e218c71b14a0bf75973833c7a1f5c3509
danilovalerio <danilo_vs@hotmail.com>
2023-05-09 10:48:23 -0300

    MUTATIONS - criando uma mutation para usar os dados de state como produtos

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,13 @@ import axios from 'axios'
 
 export default createStore({
   state: {
-    
+    produtos: [1,2,3]
   },
   mutations: {
-    
+    //o 1 argumento sempre será state para termos acesso aos dados
+    carregarProdutos(state, produtos) {
+
+    }
   },
   //actions são responsáveis por alterar as mutations que por sua vez alteram o state
   actions: {
@@ -15,7 +18,9 @@ export default createStore({
       axios
             .get('https://fakestoreapi.com/products')
             .then(response => {
-                console.log(response.data)
+              //para usar a mutacao 'carregarProdutos' tem que ser o commit
+                //console.log(response.data)
+                commit('carregarProdutos', response.data)
             })
     }
 
